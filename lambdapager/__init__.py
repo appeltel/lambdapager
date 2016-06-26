69e96267a41113b213b59d3f91eebe93cfe98e47
Eric Appelt <eric.appelt@gmail.com>
2016-06-25 20:02:37 -0500

    Refactor general package structure

---
+++ lambdapager/__init__.py
@@ -0,0 +1,13 @@
+"""
+lambdapager
+
+The stateless pager
+"""
+
+
+__title__ = 'lambdapager'
+__version__ = '0.0.0'
+__author__ = 'Eric Appelt'
+__copyright__ = 'Copyright 2016 Eric Appelt'
+
+from lambdapager.pager import LambdaPager
