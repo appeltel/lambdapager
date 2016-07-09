2d08099e965e1e85269e7c2fa2690f1aebbc2a10
Eric Appelt <eric.appelt@gmail.com>
2016-07-09 13:36:50 -0500

    Add classifiers to setup

--- setup.py
+++ setup.py
@@ -15,5 +15,14 @@ setup(
         'lambdapager_bluemix_create = lambdapager.bluemix:create_bluemix_handler',
         'lambdapager_aws_create = lambdapager.aws:create_aws_handler'
     ]},
-    install_requires = ['requests>=2.10.0', 'twilio>=5.4.0']
+    install_requires=['requests>=2.10.0', 'twilio>=5.4.0'],
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Monitoring'
+    )
 )
